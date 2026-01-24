14bd762ac9ccf09544cb6f76dae8d6f08a3a387e
RS <69071162+Arrus218@users.noreply.github.com>
2026-01-25 00:18:59 +0800

    Rename, greet, exit
    Renamed the chatbot from Duke to Ginger. Added the greeting and exit.

---
+++ src/main/java/Ginger.java
@@ -0,0 +1,7 @@
+public class Ginger {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ginger.\nWhat can I do for you?");
+        System.out.println("---------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
