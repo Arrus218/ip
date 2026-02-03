6b2d7e2650f3eb3c922ef74f6053d52a9b0885ac
RS <69071162+Arrus218@users.noreply.github.com>
2026-02-04 00:37:41 +0800

    Add abstract Command class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
-public class Command {
+public abstract class Command {
+    public abstract void execute(TaskList tasks, Ui chatBot, Storage storage) throws GingerException;
+    public boolean isExit() { return false; }
 }
