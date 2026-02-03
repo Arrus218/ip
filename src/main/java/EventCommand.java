9db920bc945671cbb8453a4273e861bd2a825d98
RS <69071162+Arrus218@users.noreply.github.com>
2026-02-04 01:20:07 +0800

    Add EventCommand class

---
+++ src/main/java/EventCommand.java
@@ -0,0 +1,21 @@
+public class EventCommand extends AddCommand {
+    private String from;
+    private String to;
+
+    public EventCommand(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws GingerException {
+        Task t = new Event(this.description, this.from, this.to);
+        super.onExecute(t, tasks, ui, storage);
+    }
+
+    @Override
+    public boolean isExit() {
+        return super.isExit();
+    }
+}
