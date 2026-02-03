6e2d18e9e59878c02bddc7ea3abc6e9216e3cfcf
RS <69071162+Arrus218@users.noreply.github.com>
2026-02-04 01:46:14 +0800

    Add MarkCommand class

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,13 @@
+public class MarkCommand extends IndexCommand {
+
+    public MarkCommand(int index) {
+        super(index);
+    }
+
+    @Override
+    protected void execute(TaskList tasks, Ui ui, Storage storage) throws GingerException {
+        Task t = tasks.getTask(super.index);
+        t.setDone();
+        super.onExecute(t, tasks, ui, storage);
+    }
+}
