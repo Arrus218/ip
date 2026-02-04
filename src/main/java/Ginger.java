500575e4b17bcbd02d65fffb02236e0a80dfbef9
RS <69071162+Arrus218@users.noreply.github.com>
2026-02-04 16:19:07 +0800

    Change Ginger class to use new classes

--- src/main/java/Ginger.java
+++ src/main/java/Ginger.java
@@ -1,17 +1,37 @@
+import java.io.IOException;
+
 public class Ginger {
     private Storage storage;
     private TaskList tasks;
-    private Ui bot;
+    private Ui ui;
 
     public Ginger(String filePath) {
         this.storage = new Storage(filePath);
-        this.tasks = new TaskList();
-        this.bot = new Ui();
+        this.ui = new Ui();
+        try {
+            this.tasks = new TaskList(storage.load());
+        } catch (GingerException e) {
+            ui.showError(e);
+            tasks = new TaskList();
+        }
     }
 
     public void run() {
-        this.bot.start();
-        this.bot.listen();
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.showSeparator(); // show the divider line ("_______")
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
+            } catch (GingerException e) {
+                ui.showError(e);
+            } finally {
+                ui.showSeparator();
+            }
+        }
     }
 
     public static void main(String[] args) {
