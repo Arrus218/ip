46f2c96647a69b61c42ad41ec62e0380e537204e
RS <69071162+Arrus218@users.noreply.github.com>
2026-02-19 12:54:03 +0800

    Add new icon to GUI

--- src/main/java/ginger/Main.java
+++ src/main/java/ginger/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -23,6 +24,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.getIcons().add(new Image (this.getClass().getResourceAsStream("/images/Icon.png")));
             stage.setTitle("Ginger");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setGinger(ginger);  // inject the Ginger instance
